ce94eb767772b118959816d96534d537b3464bc7
nemrituduk <nuv14.zn@gmail.com>
2026-03-13 16:48:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,7 @@
-// Service Worker for Linky PWA
-// Share Target API を有効にするために必要
+self.addEventListener("install",e=>{
+self.skipWaiting()
+})
 
-const CACHE_NAME = 'linky-v1';
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-// Share Target: GETリクエストをアプリにルーティングする
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-
-  // Share Target のリクエストを処理
-  // manifest の "action": "." に対応するパスへのGETリクエスト
-  if (event.request.method === 'GET' && url.searchParams.has('url')) {
-    event.respondWith(
-      (async () => {
-        // メインページを開いているクライアントを探す
-        const allClients = await self.clients.matchAll({ type: 'window' });
-
-        if (allClients.length > 0) {
-          // 既存のウィンドウがあればそこにメッセージを送ってリダイレクト
-          const client = allClients[0];
-          client.postMessage({
-            type: 'SHARE_TARGET',
-            url: url.searchParams.get('url') || url.searchParams.get('text') || '',
-            title: url.searchParams.get('title') || '',
-          });
-          client.focus();
-          return Response.redirect(url.origin + url.pathname, 303);
-        } else {
-          // ウィンドウが存在しない場合: クエリパラメータ付きでリダイレクト
-          return Response.redirect(event.request.url, 303);
-        }
-      })()
-    );
-    return;
-  }
-
-  // その他のリクエストはネットワークに通す（キャッシュなし運用でもOK）
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
+self.addEventListener("activate",e=>{
+clients.claim()
+})
